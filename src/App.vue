90b624db101cadd9e72aafa1429ab6e093d43e96
Matheus Fonseca <teus.fonseca@hotmail.com>
2023-02-11 14:04:53 -0300

    introducao da lista de pokemons e barra de pesquisa

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,39 @@
+<template>
+
+  <h1>Pokedex</h1>
+  <input type="text" v-model="filterText">
+  <ul>
+  <li v-for="(pokemon, index) in filteredPokemonList" :key="`poke-${index}`">
+  #{{ pokemon.entry_number }} - {{ pokemon.pokemon_species.name }}
+  </li>
+  </ul>
+
+</template>
+
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import {ref, computed, onMounted} from 'vue'
+
+const pokemonList = ref([])
+const filterText = ref('')
+
+const filteredPokemonList = computed(() => 
+pokemonList.value.filter(pokemon =>
+  pokemon.pokemon_species.name.includes(filterText.value)
+  )
+  )
+
+onMounted(async () => {
+  const pokeData = await fetch('https://pokeapi.co/api/v2/pokedex/2/')
+  .then(response => response.json())
+
+pokemonList.value = pokeData.pokemon_entries
+
+})
+
 </script>
 
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+
+
 
 <style scoped>
 .logo {
